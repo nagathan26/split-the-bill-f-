ded2e80a16e0370b2915b9d61d949f1031f8826d
nagathan26 <sanketnagathan@gmail.com>
2025-10-24 09:09:30 +0530

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/split-the-bill/",  // 👈 use your repo name here
+  base: "/split-the-bill-f-/",  // 👈 use your repo name here
 });
